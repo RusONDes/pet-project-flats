70939190c2cd0cb97a944a5802e2657e75704a0a
антон <anton@MacBook-Air-anton.local>
2024-07-16 03:25:46 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,112 @@
+<template>
+    <div>
+      <header>
+        <div style="width: 100%; height: 5vw;">
+          <a-flex gap="middle">
+            <a-flex gap="middle" justify='space-evenly' style="width: 50%;height: 5vw; display: flex; align-items: center;">
+              <div>
+                <h1>лого</h1>
+              </div>
+              <a-button type="link" href="/">Гланая</a-button>
+              <a-button type="link" href="/">Квартиры</a-button>
+              <a-button type="link" href="/">Контакты</a-button>
+              <a-button type="link" href="/">О нас</a-button>
+            </a-flex>
+            <div style="background-color: #2B7AF1; width: 50%; display: flex; align-items: center">
+              <a-flex gap="middle" justify='space-evenly' style="padding: 0 8px; width: 100%;">
+                <a-flex gap="5">
+                  <div class="card">
+                    <MailTwoTone style="font-size: 20px;" />
+                  </div>
+                  <span class="text">mail@yandex.ru</span>
+                </a-flex>
+                <a-flex gap="5">
+                  <div class="card">
+                    <PhoneTwoTone style="font-size: 20px;" />
+                  </div>
+                  <span class="text">+7 (919) 714-25-00</span>
+                </a-flex>
+                <div style="position: relative; bottom: 5px;">
+                  <a-button type="primary" size="middle" style="background-color: #FF7A00; border-radius: 15px;">Заказать звонок</a-button>
+                </div>
+              </a-flex>
+            </div>
+          </a-flex>
+        </div>
+      </header>
+      <main>
+        <div class="pages"> 
+    <div> pages home </div>
+
+
+        </div>
+        <div class="gradient-line"></div>
+        <div class="recurring_element ">
+           <div class="About_us">
+            <div>
+                <h1 class="span_blue">О нас</h1>
+            </div>
+             
+             <div>
+                <a-row>
+               <a-col :span="6">
+                Сегодня «Домашняя гостиница» — одна из крупнейших в Пермском крае гостиничных сетей, динамично расширяющая географический охват и повышающая качество сервиса.
+            </a-col>
+              <a-col :span="6">
+
+              </a-col>
+              <a-col :span="6">col-6</a-col>
+              <a-col :span="6">col-6</a-col>
+             </a-row>
+             </div>
+
+           </div>
+
+        </div>
+      </main>
+    </div>
+  </template>
+  
+
+<script>
+ import { MailTwoTone , PhoneTwoTone } from '@ant-design/icons-vue';
+ import { defineComponent } from 'vue';
+ export default defineComponent({
+    components: {
+        MailTwoTone,PhoneTwoTone
+    },
+  });
+</script>
+<style>
+*{
+    margin: 0;
+    padding: 0;
+}
+.card{
+    background-color: rgb(255, 255, 255);
+     border-radius: 3rem; 
+    width: 30px;
+    height: 30px;
+     text-align: center; 
+    display: flex;
+     align-items: center;
+     justify-content: center;
+     position: relative;
+     bottom: 5px;
+}
+.text{
+    color: white;
+}
+.About_us{
+    padding: 0  50px ;
+}
+.gradient-line {
+            width: 100%;
+            height: 1px;
+            background: linear-gradient(to right, rgba(255, 255, 255, 0), #f2b632, rgba(255, 255, 255, 0));
+            margin: 20px 0; /* Adjust margin as needed */
+        }
+.span_blue{
+    color: #2B7AF1;
+}
+</style>
